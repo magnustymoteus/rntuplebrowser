3559c3f94d410430650222da35449bdd6b98fd0d
Patryk Pilichowski <patryk.pilichowski@student.uantwerpen.be>
2025-07-15 10:10:03 +0200

    rntuple integration

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <ROOT/RCanvas.hxx>
-#include <ROOT/RText.hxx>
-#include <ROOT/RLine.hxx>
+#include <ROOT/RBrowser.hxx>
+#include <ROOT/RNTuple.hxx>
+#include <ROOT/RNTupleReader.hxx>
+#include <TFile.h>
 #include <TApplication.h>
 
 using namespace ROOT::Experimental;
@@ -9,10 +11,16 @@ void DisplayCanvas() {
     auto canvas = RCanvas::Create("Hello world!");
     canvas->Show();
 }
-
+void DisplayBrowser() {
+    auto browser = new ROOT::RBrowser("Hello there!");
+}
+void ReadModel(const std::string &fileName, const std::string &tupleName) {
+    auto reader = ROOT::RNTupleReader::Open(tupleName.c_str(), fileName.c_str());
+}
 int main(int argc, char** argv) {
     TApplication app("ROOT Application", &argc, argv);
     DisplayCanvas();
+    //DisplayBrowser();
     app.Run();
     return 0;
 }
