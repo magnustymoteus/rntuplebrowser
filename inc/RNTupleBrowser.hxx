5103693fd9e85621441e9e7e8e191e26d812d673
Patryk Pilichowski <pat.pilichowski@gmail.com>
2025-07-16 16:58:03 +0200

    put everything in RNTupleBrowser

--- inc/RNTupleBrowser.hxx
+++ inc/RNTupleBrowser.hxx
@@ -6,15 +6,16 @@
 
 #include <ROOT/RNTuple.hxx>
 #include <ROOT/RCanvas.hxx>
-#include <ROOT/RNTupleReader.hxx>
+#include <ROOT/RNTupleInspector.hxx>
 #include <ROOT/RNTupleDescriptor.hxx>
 using namespace ROOT::Experimental;
 class RNTupleBrowser {
     private:
-        std::shared_ptr<RCanvas> _canvas;
-        std::unique_ptr<ROOT::RNTupleReader> _reader;
+        const std::shared_ptr<RCanvas> _canvas;
+        const std::unique_ptr<RNTupleInspector> _inspector;
     public:
-        RNTupleBrowser(const std::string_view tupleName, const std::string_view storage);
+    RNTupleBrowser(const std::string_view tupleName, const std::string_view storage)
+: _canvas(RCanvas::Create("RNTupleBrowser")), _inspector(RNTupleInspector::Create(tupleName, storage)) {}
         void Browse() const;
 };
 
